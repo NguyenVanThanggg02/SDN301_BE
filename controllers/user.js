a7fe22c33d244170522e43c2aec398d89624edcc
ThangNVHE161412 <136911637+NguyenVanThanggg02@users.noreply.github.com>
2024-03-10 22:15:11 +0700

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -3,76 +3,92 @@
 import { userDAO } from "../repositories/index.js";
 
 const loginUser = async (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    try {
-        // Lấy người dùng từ cơ sở dữ liệu dựa trên tên người dùng
-        const user = await userDAO.getUserByUsername(username);
-        // Kiểm tra nếu người dùng tồn tại
-        if (!user) {
-            return res.status(401).json({ message: "Username is incorrect" });
-        }
-
-        // So sánh mật khẩu được cung cấp với mật khẩu đã được băm
-        // const isPasswordValid = await bcrypt.compare(password, user.password);
-        // console.log(password);
-        if (password !== user.password) {
-            return res.status(401).json({ message: "Password is incorrect" });
-        }
-
-        // Đăng nhập thành công, trả về thông tin người dùng hoặc token
-        res.status(200).json({ message: "Login successful", user });
-    } catch (error) {
-        res.status(500).json({ error: error.toString() });
+  try {
+    // Lấy người dùng từ cơ sở dữ liệu dựa trên tên người dùng
+    const user = await userDAO.getUserByUsername(username);
+    // Kiểm tra nếu người dùng tồn tại
+    if (!user) {
+      return res.status(401).json({ message: "Username is incorrect" });
     }
-}
+
+    // So sánh mật khẩu được cung cấp với mật khẩu đã được băm
+    // const isPasswordValid = await bcrypt.compare(password, user.password);
+    // console.log(password);
+    if (password !== user.password) {
+      return res.status(401).json({ message: "Password is incorrect" });
+    }
+
+    // Đăng nhập thành công, trả về thông tin người dùng hoặc token
+    res.status(200).json({ message: "Login successful", user });
+  } catch (error) {
+    res.status(500).json({ error: error.toString() });
+  }
+};
 
 // Fetch all products
 const getAllUsers = async (req, res) => {
-    try {
-        const allUsers = await userDAO.fetchAll();
-        res.status(200).json(allUsers);
-    } catch (error) {
-        res.status(500).json({
-            error: error.toString()
-        });
-    }
-}
+  try {
+    const allUsers = await userDAO.fetchAll();
+    res.status(200).json(allUsers);
+  } catch (error) {
+    res.status(500).json({
+      error: error.toString(),
+    });
+  }
+};
+
+const getUserByUsername = async (req, res) => {
+  try {
+    const username = await userDAO.getUserByUsername(req.params.username);
+    res.status(200).json(username);
+  } catch (error) {
+    res.status(500).json({
+      error: error.toString(),
+    });
+  }
+};
 
 // Create a new product
 const createUser = async (req, res) => {
-    // Lấy dữ liệu từ yêu cầu
-    try {
-        const { username, password } = req.body;
-        const result = await userDAO.create({ username, password });
-        res.status(201).json(result);
-    } catch (error) {
-        res.status(500).json({
-            error: error.toString()
-        });
-    }
-}
+  // Lấy dữ liệu từ yêu cầu
+  try {
+    const { username, password } = req.body;
+    const result = await userDAO.create({ username, password });
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({
+      error: error.toString(),
+    });
+  }
+};
 
-// Fetch all products by id
-// const getUserById = async (req, res) => {
-//     try {
-//         const productId = req.params.id
-//         const allUsers = await userDAO.fetchAllUserById(productId);
-//         if (allUsers) {
-//             res.status(200).json({
-//                 message: "Load data successfully",
-//                 data: allUsers,
-//             })
-//         } else {
-//             res.status(404).json('Not found')
-//         }
-//     } catch (error) {
-//         res.status(500).json({
-//             message: error.toString()
-//         })
-//     }
-// }
+const updateUser = async (req, res) => {
+  try {
+    res.status(200).json(await userDAO.updateUser(req.params.id, req.body));
+  } catch (error) {
+    res.status(500).json({
+      error: error.toString(),
+    });
+  }
+};
+const deleteUser = async (req, res) => {
+  try {
+    const deleteUser = await userDAO.deleteUser(req.params.id);
+    res.status(200).json(deleteUser);
+  } catch (error) {
+    res.status(500).json({
+      error: error.toString(),
+    });
+  }
+};
 
 export default {
-    getAllUsers, createUser, loginUser
-}
+  getAllUsers,
+  getUserByUsername,
+  createUser,
+  loginUser,
+  updateUser,
+  deleteUser,
+};
