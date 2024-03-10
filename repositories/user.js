0fa7dcd366419e22dcbc4ac8b85040a6ddc598db
ThangNVHE161412 <136911637+NguyenVanThanggg02@users.noreply.github.com>
2024-03-10 22:16:41 +0700

    Update user.js

--- repositories/user.js
+++ repositories/user.js
@@ -1,24 +1,21 @@
-
 import Users from "../models/users.js";
 
-
 const create = async ({ username, password }) => {
-    try {
-        const newUser = await Users.create({ username, password });
-        return newUser._doc;
-    } catch (error) {
-        throw new Error(error.toString())
-    }
-}
-
+  try {
+    const newUser = await Users.create({ username, password });
+    return newUser._doc;
+  } catch (error) {
+    throw new Error(error.toString());
+  }
+};
 
 const fetchAll = async () => {
-    try {
-        return await Users.find({}).exec();
-    } catch (error) {
-        throw new Error(error.toString());
-    }
-}
+  try {
+    return await Users.find({}).exec();
+  } catch (error) {
+    throw new Error(error.toString());
+  }
+};
 
 //R:Fetch all  new Product by Id
 
@@ -31,12 +28,48 @@ const fetchAll = async () => {
 //     }
 // }
 const getUserByUsername = async (username) => {
-    try {
-        return await Users.findOne({ username: username }).exec();
-    } catch (error) {
-        throw new Error(error.toString());
-    }
-}
+  try {
+    return await Users.findOne({ username: username }).exec();
+  } catch (error) {
+    throw new Error(error.toString());
+  }
+};
+const updateUser = async (
+  id,
+  { full_name, username, Email, password, gender, birthday, phone, address }
+) => {
+  try {
+    return await Users.findByIdAndUpdate(
+      { _id: id },
+      {
+        full_name,
+        username,
+        Email,
+        password,
+        gender,
+        birthday,
+        phone,
+        address,
+      },
+      { new: true }
+    );
+  } catch (error) {
+    throw new Error(error.toSring());
+  }
+};
+const deleteUser = async (id) => {
+  try {
+    const deleteUser = await Users.findByIdAndDelete({ _id: id }).exec();
+    return deleteUser._doc;
+  } catch (error) {
+    throw new Error(error.toSring());
+  }
+};
+
 export default {
-    create, fetchAll, getUserByUsername
-}
+  create,
+  fetchAll,
+  getUserByUsername,
+  updateUser,
+  deleteUser,
+};
