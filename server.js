99db66031a31ec0fe096371fb9f09aecbd85c694
haudd1705 <haudd1705@gmail.com>
2024-03-18 01:37:57 +0700

    aaa

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use('/sizes', sizeRouter)
 app.use('/images', imageRouter)
 app.use('/inventory', inventoryRouter)
 
+// app.use('/brands', brandRouter)
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
